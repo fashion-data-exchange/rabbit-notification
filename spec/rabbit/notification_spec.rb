ce6651dd7ed100cf16e941bd400064334f7303c1
Felix Langenegger <f.langenegger@fadendaten.ch>
2017-09-12 14:13:37 +0200

    Renamed Gem

--- spec/rabbit/notification_spec.rb
+++ spec/rabbit/notification_spec.rb
@@ -4,4 +4,16 @@ RSpec.describe FDE::Rabbit::Notification do
   it "has a version number" do
     expect(FDE::Rabbit::Notification::VERSION).not_to be nil
   end
+
+  describe 'configuration' do
+    it 'is of Type FDE::S3Client::Config' do
+      expect(subject.config).to be_a(FDE::Rabbit::Notification::Config)
+    end
+
+    it 'yields the config block' do
+      expect do |b|
+        subject.configure(&b)
+      end.to yield_with_args
+    end
+  end
 end
