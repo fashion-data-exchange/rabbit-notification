9b479c14af367a0225a2fc92e5c8d76c97a63142
Felix Langenegger <f.langenegger@fadendaten.ch>
2017-09-12 09:20:33 +0200

    Init Commit

---
+++ lib/rabbit/notification.rb
@@ -0,0 +1,7 @@
+require "rabbit/notification/version"
+
+module Rabbit
+  module Notification
+    # Your code goes here...
+  end
+end
---
+++ lib/rabbit/notification/version.rb
@@ -0,0 +1,5 @@
+module Rabbit
+  module Notification
+    VERSION = "0.1.0"
+  end
+end
